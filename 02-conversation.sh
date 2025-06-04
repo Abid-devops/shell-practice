c921be91d9d76dc1443fadac9056544e6aaeb526
fardeenabid <info@devops23.com>
2025-06-04 10:54:16 +0530

    coversation created

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump:: Hey Musk, How are You?"
+echo "Musk:: Hello Trump, Iam fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "Musk:: Nothing, just going to Mars now, will you come?"
+echo "Trump:: Sorry, you carry on! I will come once you come back"

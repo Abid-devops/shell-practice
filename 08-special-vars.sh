adea1fe78086b893c1a22d16f130a794c350e8a5
fardeenabid <info@devops23.com>
2025-07-07 12:07:50 +0530

    hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "Number odf variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running the script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"

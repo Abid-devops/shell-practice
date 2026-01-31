0433de75efe378e74b136c19fd9bc5fa07ef9f5b
fardeenabid <info@devops23.com>
2026-01-31 12:57:59 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,3 +5,4 @@ echo "Musk:: Hello Trump, I am fine. How are you doing"
 echo "Trump: I am fine too. What's up?"
 echo "Musk:: Nothing, just going to Mars now, will you come?"
 echo "Trump:: Sorry, you carry on! I will come once you come back"
+echo hi iam abid i want to become a pro in DevOps
